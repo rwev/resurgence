ccb5b5e5fa5b4d9bd3f30e6b6dff7905141bca6f
Ryan <rwev@rwev.dev>
2020-06-26 14:50:47 -0600

    update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version="0.1.0",
     description="Restart a process when files are changed",
     author="rwev",
-    author_email="rwev@protonmail.ch",
+    author_email="rwev@rwev.dev",
     url="https://github.com/rwev/resurgence",
     packages=["resurgence"],
     entry_points={"console_scripts": ["resurgence=resurgence.resurgence:main"]},
